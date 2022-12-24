d2df7e49afb0056eee178447d2269cfd1967abfe
Hari Kishan <harikishan2802@gmail.com>
2022-12-24 17:57:02 +0530

    creating backed s3 bucket

--- example/backend.tf
+++ example/backend.tf
@@ -1,6 +1,6 @@
 terraform {
-  backend "s3"
-  bucket = "terraform-hk63"
-  key    = "example/terraform.tfstate"
-  region = "us-east-1"
+  backend ="s3"
+  bucket  = "terraform-hk63"
+  key     = "example/terraform.tfstate"
+  region  = "us-east-1"
 }
